a29bd9159c9f1f8a6be084327f06ac72bf6d3385
Adam Brown <adam@novoda.com>
2013-01-16 20:04:27 +0000

    adding bottom overflow button (backported)

---
+++ app/src/com/adam/rvc/fragment/ActionBarFragment.java
@@ -0,0 +1,45 @@
+package com.adam.rvc.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import com.adam.rvc.R;
+import com.adam.rvc.view.org.holoeverywhere.PopupMenu;
+
+public class ActionBarFragment extends BaseFragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_bottom_action_bar, container, false);
+        initButton(view);
+        return view;
+    }
+
+    private void initButton(View view) {
+        ImageButton imageButton = (ImageButton) view.findViewById(R.id.options_button);
+        imageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        showPopUpMenu(view);
+    }
+
+    private void showPopUpMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(context, v);
+        popupMenu.inflate(R.menu.settings_menu);
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(com.actionbarsherlock.view.MenuItem item) {
+                return false;
+            }
+        });
+
+        popupMenu.show();
+    }
+
+}
---
+++ app/src/com/adam/rvc/view/org/holoeverywhere/PopupMenu.java
@@ -0,0 +1,101 @@
+package com.adam.rvc.view.org.holoeverywhere;
+
+
+import android.content.Context;
+import android.view.View;
+import com.actionbarsherlock.internal.view.menu.MenuBuilder;
+import com.actionbarsherlock.internal.view.menu.MenuPopupHelper;
+import com.actionbarsherlock.internal.view.menu.MenuPresenter;
+import com.actionbarsherlock.internal.view.menu.SubMenuBuilder;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+public class PopupMenu implements MenuBuilder.Callback, MenuPresenter.Callback {
+    public interface OnDismissListener {
+        public void onDismiss(PopupMenu menu);
+    }
+
+    public interface OnMenuItemClickListener {
+        public boolean onMenuItemClick(MenuItem item);
+    }
+
+    private final View mAnchor;
+    private final Context mContext;
+    private final MenuBuilder mMenu;
+    private final MenuPopupHelper mPopup;
+
+    private OnDismissListener mDismissListener;
+    private OnMenuItemClickListener mMenuItemClickListener;
+
+    public PopupMenu(Context context, View anchor) {
+        mContext = context;
+        mMenu = new MenuBuilder(context);
+        mMenu.setCallback(this);
+        mAnchor = anchor;
+        mPopup = new MenuPopupHelper(context, mMenu, mAnchor);
+        mPopup.setCallback(this);
+    }
+
+    public void dismiss() {
+        mPopup.dismiss();
+    }
+
+    public Menu getMenu() {
+        return mMenu;
+    }
+
+    public MenuInflater getMenuInflater() {
+        return new MenuInflater(mContext);
+    }
+
+    public void inflate(int menuRes) {
+        getMenuInflater().inflate(menuRes, mMenu);
+    }
+
+    @Override
+    public void onCloseMenu(MenuBuilder menu, boolean allMenusAreClosing) {
+        if (mDismissListener != null) {
+            mDismissListener.onDismiss(this);
+        }
+    }
+
+    public void onCloseSubMenu(SubMenuBuilder menu) {
+    }
+
+    @Override
+    public boolean onMenuItemSelected(MenuBuilder menu, MenuItem item) {
+        if (mMenuItemClickListener != null) {
+            return mMenuItemClickListener.onMenuItemClick(item);
+        }
+        return false;
+    }
+
+    @Override
+    public void onMenuModeChange(MenuBuilder menu) {
+    }
+
+    @Override
+    public boolean onOpenSubMenu(MenuBuilder subMenu) {
+        if (subMenu == null) {
+            return false;
+        }
+        if (!subMenu.hasVisibleItems()) {
+            return true;
+        }
+        new MenuPopupHelper(mContext, subMenu, mAnchor).show();
+        return true;
+    }
+
+    public void setOnDismissListener(OnDismissListener listener) {
+        mDismissListener = listener;
+    }
+
+    public void setOnMenuItemClickListener(OnMenuItemClickListener listener) {
+        mMenuItemClickListener = listener;
+    }
+
+    public void show() {
+        mPopup.show();
+    }
+}
