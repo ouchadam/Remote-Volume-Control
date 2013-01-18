dc29f56497b6da11120eee8d61dbc1845b43b0a9
Adam Brown <adam@novoda.com>
2013-01-18 20:12:47 +0000

    adding google analytics

--- app/src/com/adam/rvc/activity/RVCActivity.java
+++ app/src/com/adam/rvc/activity/RVCActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.google.analytics.tracking.android.EasyTracker;
 
 public abstract class RVCActivity extends SherlockFragmentActivity {
 
@@ -11,11 +12,24 @@ public abstract class RVCActivity extends SherlockFragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initWindow();
+        EasyTracker.getInstance().setContext(this);
     }
 
     private void initWindow() {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EasyTracker.getInstance().activityStart(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EasyTracker.getInstance().activityStop(this);
     }
 
 }
