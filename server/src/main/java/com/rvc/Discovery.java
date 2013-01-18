d8a199048d4921f5389ff7a6d408adffe35c0287
Adam Brown <adam@novoda.com>
2013-01-18 20:19:53 +0000

    renaming jmdns service

--- server/src/main/java/com/rvc/Discovery.java
+++ server/src/main/java/com/rvc/Discovery.java
@@ -5,6 +5,8 @@ import com.rvc.server.ServerSettings;
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class Discovery {
 
@@ -24,7 +26,16 @@ public class Discovery {
     }
 
     ServiceInfo createService() {
-        return ServiceInfo.create(SERVICE_TYPE, "Test Service", settings.getPort(), "test service");
+        return ServiceInfo.create(SERVICE_TYPE, createServiceName(), settings.getPort(), "RVC");
+    }
+
+    private String createServiceName() {
+        try {
+            return "RVC " + InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Couldn't retrieve host name");
+        }
     }
 
     private void registerService(ServiceInfo testService) throws IOException {
